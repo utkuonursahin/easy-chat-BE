6d0f8f75f85bd4448c60dacd30412d264e1815af
utkuonursahin <sahinutkuonur@gmail.com>
2024-08-25 10:47:35 +0300

    feat: implement live typing notification

--- src/main/java/me/utku/easychatbe/enums/WebSocketEvent.java
+++ src/main/java/me/utku/easychatbe/enums/WebSocketEvent.java
@@ -10,7 +10,9 @@ public enum WebSocketEvent {
     JOIN_ROOM("join_room"),
     LEAVE_ROOM("leave_room"),
     SEND_MESSAGE("send_message"),
-    GET_MESSAGE("get_message");
+    GET_MESSAGE("get_message"),
+    TYPING("typing"),
+    STOP_TYPING("stop_typing");
 
     private final String event;
 }
--- src/main/java/me/utku/easychatbe/socket/SocketConfigurer.java
+++ src/main/java/me/utku/easychatbe/socket/SocketConfigurer.java
@@ -3,8 +3,9 @@ package me.utku.easychatbe.socket;
 import com.corundumstudio.socketio.SocketIOServer;
 import lombok.extern.slf4j.Slf4j;
 import me.utku.easychatbe.chatroom.ChatRoomDto;
-import me.utku.easychatbe.message.MessageDto;
 import me.utku.easychatbe.enums.WebSocketEvent;
+import me.utku.easychatbe.message.MessageDto;
+import me.utku.easychatbe.user.UserDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,5 +17,7 @@ public class SocketConfigurer {
         socketIOServer.addEventListener(WebSocketEvent.JOIN_ROOM.getEvent(), ChatRoomDto.class, socketService.onRoomJoined());
         socketIOServer.addEventListener(WebSocketEvent.LEAVE_ROOM.getEvent(), ChatRoomDto.class, socketService.onRoomLeaved());
         socketIOServer.addEventListener(WebSocketEvent.SEND_MESSAGE.getEvent(), MessageDto.class, socketService.onMessageReceived());
+        socketIOServer.addEventListener(WebSocketEvent.TYPING.getEvent(), UserDto.class, socketService.onTyping());
+        socketIOServer.addEventListener(WebSocketEvent.STOP_TYPING.getEvent(), UserDto.class, socketService.onStopTyping());
     }
 }
